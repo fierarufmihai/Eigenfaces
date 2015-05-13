356a2e7d19507bf840090c2d964da6683c5d2b8c
Mihai Fieraru <m.fieraru@jacobs-university.de>
2015-05-13 23:17:39 +0200

    update readme + add download script

---
+++ preprocessing/download.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import urllib
+
+links = {"im1m": "http://static4.wikia.nocookie.net/__cb20130808202627/marvelmovies/images/e/ee/Brad-Pitt.jpg", \
+ "im2m": "http://images2.fanpop.com/image/photos/9000000/Matt-Damon-matt-damon-9040366-1024-768.jpg", \
+ "im3m": "http://upload.wikimedia.org/wikipedia/commons/0/0d/Christian_Bale_2009.jpg", \
+ "im4m": "http://cdn.cnwimg.com/wp-content/uploads/2009/10/Tom-Hanks-768x1024.jpg", \
+ "im5m": "http://images.askmen.com/photos/ryan-reynolds/86365.jpg", \
+ "im1f": "http://www.interiordev.com/imagedir/jessica-beils-most-stunning-looks.jpg", \
+ "im2f": "http://celebritiesexercise.com/wp-content/uploads/2013/05/mila-kunis.jpg", \
+ "im3f": "http://ucesy-sk.happyhair.sk/celebrity_img/longoria2mj2509.jpg", \
+ "im4f": "http://fast.celebrityphoto.com/img/NatalieHall112711-SD21.JPG", \
+ "im5f": "http://img005.lazygirls.info/people/veronica_hamel/veronica_hamel_18_veronica_ca_1990_bKzbTVq.sized.jpg"}
+
+for im_name in links:
+	urllib.urlretrieve(links[im_name], "data/raw/" + im_name + ".jpg")
